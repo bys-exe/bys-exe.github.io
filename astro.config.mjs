282e1aea635e19727d1d90bf2a55e6ea02ab9828
bys_exe <batchuyugasai@gmail.com>
2026-07-26 18:56:01 +0530

    Update site for pages
    modified some things for github pages to work

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://bys-exe.github.io/yuga',
+  site: 'https://bys-exe.github.io',
+  base: '/yuga',
   devToolbar: {
     enabled: false
   },
