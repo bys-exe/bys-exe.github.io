95d165d89083da128815d53c7d3270fe00522714
bys_exe <batchuyugasai@gmail.com>
2026-08-03 19:28:54 +0530

    Update rss

--- src/lib/rss.ts
+++ src/lib/rss.ts
@@ -1,4 +1,6 @@
-import { getCollection } from "astro:content";
+import { getCollection, render } from "astro:content";
+import type { CollectionEntry } from "astro:content";
+import { experimental_AstroContainer as AstroContainer } from "astro/container";
 
 const escapeXml = (str: string) =>
     str
@@ -8,6 +10,16 @@ const escapeXml = (str: string) =>
         .replace(/"/g, "&quot;")
         .replace(/'/g, "&apos;");
 
+async function renderContent(
+    post: CollectionEntry<"blog">,
+    base: string,
+    container: AstroContainer,
+): Promise<string> {
+    const { Content } = await render(post);
+    const html = await container.renderToString(Content);
+    return html.replace(/(src|href)="\//g, `$1="${base}/`);
+}
+
 export async function generateRss(
     base: string,
     feedPath = "/rss.xml",
@@ -16,23 +28,28 @@ export async function generateRss(
         (a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf(),
     );
 
-    const items = posts
-        .map((post) => {
-            const link = `${base}/blog/${post.id}`;
-            return [
+    const container = await AstroContainer.create();
+
+    const items = [];
+    for (const post of posts) {
+        const link = `${base}/blog/${post.id}`;
+        const content = await renderContent(post, base, container);
+        items.push(
+            [
                 "    <item>",
                 `      <title>${escapeXml(post.data.title)}</title>`,
                 `      <link>${link}</link>`,
                 `      <guid>${link}</guid>`,
                 `      <pubDate>${post.data.pubDate.toUTCString()}</pubDate>`,
                 `      <description>${escapeXml(post.data.description)}</description>`,
+                `      <content:encoded><![CDATA[${content}]]></content:encoded>`,
                 "    </item>",
-            ].join("\n");
-        })
-        .join("\n");
+            ].join("\n"),
+        );
+    }
 
     return `<?xml version="1.0" encoding="UTF-8"?>
-<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+<rss version="2.0" xmlns:content="http://purl.org/rss/1.0/modules/content/" xmlns:atom="http://www.w3.org/2005/Atom">
   <channel>
     <title>Yuga Sai - Blog</title>
     <link>${base}/blog</link>
@@ -40,7 +57,12 @@ export async function generateRss(
     <language>en</language>
     <lastBuildDate>${new Date().toUTCString()}</lastBuildDate>
     <atom:link href="${base}${feedPath}" rel="self" type="application/rss+xml"/>
-${items}
+    <image>
+      <url>${base}/images/pfp.jpg</url>
+      <title>Yuga Sai - Blog</title>
+      <link>${base}/blog</link>
+    </image>
+${items.join("\n")}
   </channel>
 </rss>
 `;
